a0889a27ea88912e17e0c5825ea4b512ba76744f
Amar <amar.ustavdija@gmail.com>
2022-10-14 00:13:06 +0200

    quick fix

--- loginapp/urls.py
+++ loginapp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.createUserView, name='register'),
+    path('', views.register, name='register'),
     ]
 
 # need to insert name of the URL
