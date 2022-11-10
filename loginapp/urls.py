f31140630c53cf9ca803e3ef19eb722baefed52c
Amar <amar.ustavdija@gmail.com>
2022-11-10 06:37:16 +0100

    test if wraper is called corectly

--- loginapp/urls.py
+++ loginapp/urls.py
@@ -1,9 +1,10 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.home_view, name='home'),
-    path('loggedin/', views.profile_view, name='logged_in'),
+    path('loggedin/', login_required(views.profile_view), name='logged_in'),
     path('register/', views.register, name='register'),
     ]
 
